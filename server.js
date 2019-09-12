f5da8113ad141e67f26401549196db57a21f5717
Nolly Sasha <nsnko3@student.monash.edu>
2019-09-12 21:14:22 +0200

    comit

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 let express = require('express');
+const mongodb = require("mongodb");
 let bodyParser = require('body-parser');
 let app = express();
 
-let db = [];
+//Configure MongoDB
+const MongoClient = mongodb.MongoClient;
+{useUnifiedTopology : true}
+
+// Connection URL
+const url = "mongodb://localhost:27017/";
+
+let db;
+
+//Connect to mongoDB server
+MongoClient.connect(url, { useNewUrlParser: true },
+    function (err, client) {
+        if (err) {
+            console.log("Err  ", err);
+        } else {
+            console.log("Connected successfully to server");
+            db = client.db("fit2095db");
+        }
+});
+
 
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
@@ -25,31 +45,82 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-//GET callback method to render the html page to add the task
-app.get('/addtask', function (req, res) {
-    res.render('addtask.html');
+//GET callback method to render the html page and form to add the task
+app.get('/addtasks', function (req, res) {
+    res.render('addtasks.html');
 });
 
 //POST method to handle POST requests from the input field
 app.post('/newtask', function (req, res) {
     let taskdetails = req.body;
-    let newTask = {
-        name: taskdetails.taskname,
+    taskdetails.taskId = getNewId();
+    taskdetails.taskstatus = "InProgress"
+    db.collection('tasks').insertOne({ 
+        id: taskdetails.taskId, 
+        name: taskdetails.taskname, 
+        assignto: taskdetails.taskassigned, 
         due: taskdetails.taskdue,
-        description: taskdetails.taskdescription
-    };
-
-    db.push(newTask); //Pushes the input data from the form into a database array
-    console.log(taskdetails.taskname);
-    console.log(taskdetails.taskdue);
-    console.log(taskdetails.taskdescription);
-    res.redirect('listtasks'); //Redirects to the List Tasks method and page to show the current task added
+        status: taskdetails.taskstatus,
+        description: taskdetails.taskdescription 
+    });
+    res.redirect('listtasks'); // redirect the client to list tasks page
 });
+    
 
 //GET method for the request to print out all tasks in the database
 app.get('/listtasks', function (req, res) {
-    let taskdb = db; //Declaring the database locally to be ale to call it using the HTML page
-    res.render('listtasks.html', {taskdb:db});
+    db.collection('tasks').find({}).toArray(function (err, data) {
+        res.render('listtasks', { taskdb: data });
+    });
+
+    // let taskdb = db; //Declaring the database locally to be ale to call it using the HTML page
+    // res.render('listtasks.html', {taskdb:db});
 });
+
+//Delete Task: 
+//GET request: send the page to the client to enter the user's name
+app.get('/deletetask', function (req, res) {
+    res.render('deletetask.html');
+});
+
+//POST request: receive the user's name and do the delete operation 
+app.post('/deletetaskdata', function (req, res) {
+    let taskdetails = req.body;
+    let filter = { id: parseInt(taskdetails.taskId) };
+    db.collection('tasks').deleteOne(filter);
+    res.redirect('listtasks'); // redirect the client to list users page
+});
+
+app.post('/deletealltasks', function (req, res) {
+    db.collection('tasks').deleteMany({status: 'Completed'});
+    res.redirect('listtasks');
+});
+
+//Update Task:
+app.get('/updatetask', function (req, res) {
+    res.render('updatetask.html');
+});
+
+//POST method to change status of task from InProgress to Completed
+app.post('/updatetaskdata', function (req, res) {
+    let taskdetails = req.body;
+    let filter = { id: parseInt(taskdetails.taskId) };
+    let update = { $set: { status: 'Completed' } };
+    db.collection('tasks').updateOne(filter, update);
+    res.redirect('/listtasks');// redirect the client to list tasks page
+});
+
+//POST method to change status of task from Completed to InProgress
+app.post('/updatecompletedtask', function (req, res) {
+    let taskdetails = req.body;
+    let filter = { id: parseInt(taskdetails.taskId) };
+    let update = { $set: { status: 'InProgress' } };
+    db.collection('tasks').updateOne(filter, update);
+    res.redirect('/listtasks');// redirect the client to list tasks page
+});
+
+function getNewId() {
+    return (Math.floor(100000 + Math.random() * 900000));
+};
 
 app.listen(8080);
